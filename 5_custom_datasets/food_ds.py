da630b96c345ec5bff6f2dd2c345c5b74bfda225
Alberto Trabacchin <alberto.trabacchin@gmail.com>
2023-11-17 11:10:29 +0900

    Added function to check and download the small dataset.

--- 5_custom_datasets/food_ds.py
+++ 5_custom_datasets/food_ds.py
@@ -1,10 +1,28 @@
 import torch
 import torchvision
+from pathlib import Path
+import requests
+import zipfile
+
+def download_dataset(download_path, dataset_path):
+    if dataset_path.is_dir():
+        print("Dataset path already exists: ", dataset_path)
+    else:
+        print("Dataset path does not exist")
+        dataset_path.mkdir(parents = True, exist_ok = True)
+
+        with open(download_path / "pizza_steak_sushi.zip", "wb") as f:
+            request = requests.get("https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip")
+            print("Downloading pizza, steak, sushi dataset as pizza_steak_sushi.zip...")
+            f.write(request.content)
+        
+        with zipfile.ZipFile(dataset_path / "pizza_steak_sushi.zip", "r") as zip_f:
+            print("Unzipping dataset to ", dataset_path)
+            zip_f.extractall(dataset_path)
+
 
 if __name__ == "__main__":
-    train_dataset = torchvision.datasets.Food101(
-        root = "datasets/Food101/",
-        download = True,
-        transform = torchvision.transforms.ToTensor(),
-        split = "train"
-    )
+    DS_DOWNL_PATH = Path("datasets/")
+    DS_PATH = DS_DOWNL_PATH / "pizza_steak_sushi"
+    download_dataset(DS_DOWNL_PATH, DS_PATH)
+    
