ca0bf310b8f19ad80df3d191cf2f7bd5e6ecc519
Alberto Trabacchin <alberto.trabacchin@gmail.com>
2023-11-17 11:52:46 +0900

    Created PyTorch dataset from images in DS folder.

--- 5_custom_datasets/food_ds.py
+++ 5_custom_datasets/food_ds.py
@@ -1,8 +1,13 @@
 import torch
-import torchvision
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
 from pathlib import Path
 import requests
 import zipfile
+import random
+import os
+import matplotlib.pyplot as plt
+import PIL.Image as Image
 
 def download_dataset(download_path, dataset_path):
     if dataset_path.is_dir():
@@ -21,8 +26,54 @@ def download_dataset(download_path, dataset_path):
             zip_f.extractall(dataset_path)
 
 
+def plot_transformed_images(dataset_path, transform, n = 3, seed = 42):
+    random.seed(seed)
+    image_paths = list(dataset_path.glob("*/*/*.jpg"))
+    random_image_paths = random.sample(image_paths, k = n)
+    for image_path in random_image_paths:
+        image = Image.open(image_path)
+        transformed_image = transform(image).permute(1, 2, 0)
+        fig, ax = plt.subplots(1, 2)
+        ax[0].imshow(image)
+        ax[0].set_title("Original image")
+        ax[0].axis("off")
+        ax[1].imshow(transformed_image)
+        ax[1].set_title("Transformed image")
+        ax[1].axis("off")
+
+
 if __name__ == "__main__":
     DS_DOWNL_PATH = Path("datasets/")
     DS_PATH = DS_DOWNL_PATH / "pizza_steak_sushi"
     download_dataset(DS_DOWNL_PATH, DS_PATH)
+
+    data_transform = transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.RandomHorizontalFlip(p = 0.5),
+        transforms.ToTensor()
+    ])
+
+    plot_transformed_images(DS_PATH, data_transform)
+
+    # Create datasets
+    train_data = datasets.ImageFolder(
+        root = DS_PATH / "train",
+        transform = data_transform,
+        target_transform = None
+    )
+    test_data = datasets.ImageFolder(
+        root = DS_PATH / "test",
+        transform = data_transform,
+        target_transform = None
+    )
+    class_dict = train_data.class_to_idx
+    print(train_data)
+    print(class_dict)
+    print(train_data[0])
+
+    # Create dataloaders
+
+
+    # plt.show()
+
     
